c7e1bbbf501737ce0ddc5b0edab486366a439551
SpacingBat3 <git@spacingbat3.anonaddy.com>
2024-09-03 17:48:17 +0200

    compat: "Unregister" `on_joinplayer` callback.
    This refactors the code, so after executing the callback to get access
    to ObjectRef, it "unregisters" itself by replacing itself in
    `registered_on_joinplayers` table with noop function.

--- compat/init.lua
+++ compat/init.lua
@@ -1,27 +1,31 @@
--- Whenever function has been registered
-local mod_set = false
+local function unregister_callback(callback)
+    local i = table.indexof(minetest.registered_on_joinplayers, callback)
+    -- It is currently unsafe to remove registered callbacks,
+    -- replace them with noop function instead.
+    minetest.registered_on_joinplayers[i] = function() end
+end
 
-minetest.register_on_joinplayer(function(player)
+local function callback(player)
+    unregister_callback(callback)
     local ObjRef = getmetatable(player)
-    if not mod_set then
-        coreanim.wrap_fn(player,"set_bone_position")
-        if table.indexof(minetest.get_modnames(),"mcl_player") >= 0 then
-            -- MCL games require separate replacement function, as those
-            -- tend to break a bit with the current API logic.
-            -- This is a dirty hack, but at least it works.
-            local oldfn = player.set_bone_position
-            local bone_called = {}
-            ObjRef.set_bone_position = function (player,bone,position,rotation)
-                if bone_called[bone] then
-                    coreanim.set_bone_position(player,bone,position,rotation)
-                else
-                    oldfn(player,bone,position,rotation)
-                    bone_called[bone] = true
-                end
+    coreanim.wrap_fn(player,"set_bone_position")
+    if table.indexof(minetest.get_modnames(),"mcl_player") >= 0 then
+        -- MCL games require separate replacement function, as those
+        -- tend to break a bit with the current API logic.
+        -- This is a dirty hack, but at least it works.
+        local oldfn = player.set_bone_position
+        local bone_called = {}
+        ObjRef.set_bone_position = function (player,bone,position,rotation)
+            if bone_called[bone] then
+                coreanim.set_bone_position(player,bone,position,rotation)
+            else
+                oldfn(player,bone,position,rotation)
+                bone_called[bone] = true
             end
-        else
-            ObjRef.set_bone_position = coreanim.set_bone_position
         end
-        mod_set = true
+    else
+        ObjRef.set_bone_position = coreanim.set_bone_position
     end
-end)
+end
+
+minetest.register_on_joinplayer(callback)
