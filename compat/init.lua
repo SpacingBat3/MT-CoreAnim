3d2f93b26cd3ada82adcab8bb1f6d27311ef412a
SpacingBat3 <git@spacingbat3.anonaddy.com>
2024-09-10 13:29:58 +0200

    compat: Initial settings implementation.
    • Implement configuration for game-specific workarounds.
    • Implement configuration for API override.
    • Implement workaround for NodeCore games.
    • Also allow user to override set_bone_override API.
    • Also shim missing APIs for older/newer game engines.

--- compat/init.lua
+++ compat/init.lua
@@ -1,3 +1,12 @@
+local shim = minetest.settings:get_bool("coreanim_compat.shim",true)
+local flags = minetest.settings:get_flags("coreanim_compat.flags") or { set_bone_position = true }
+local workaround_mcl = minetest.settings:get_bool("coreanim_compat.fixes.mcl",true)
+local workaround_mcl = minetest.settings:get_bool("coreanim_compat.fixes.nc",true)
+
+local function mod_loaded(name)
+    return table.indexof(minetest.get_modnames(),name) >= 0
+end
+
 local function unregister_callback(callback)
     local i = table.indexof(minetest.registered_on_joinplayers, callback)
     -- It is currently unsafe to remove registered callbacks,
@@ -8,23 +17,41 @@ end
 local function callback(player)
     unregister_callback(callback)
     local ObjRef = getmetatable(player)
-    coreanim.wrap_fn(player,"set_bone_position")
-    if table.indexof(minetest.get_modnames(),"mcl_player") >= 0 then
-        -- MCL games require separate replacement function, as those
-        -- tend to break a bit with the current API logic.
-        -- This is a dirty hack, but at least it works.
-        local oldfn = player.set_bone_position
-        local bone_called = {}
-        ObjRef.set_bone_position = function (player,bone,position,rotation)
-            if bone_called[bone] then
-                coreanim.set_bone_position(player,bone,position,rotation)
+    local position_api = coreanim.register_fn(player,"set_bone_position")
+    local override_api = coreanim.register_fn(player,"set_bone_override")
+    if (shim and not position_api) or flags.set_bone_position then
+        if mod_loaded("mcl_player") and workaround_mcl then
+            -- MCL games require separate replacement function, as those
+            -- tend to break a bit with the current API logic.
+            -- This is a dirty hack, but at least it works.
+            local oldfn = player.set_bone_position
+            local bone_called = {}
+            ObjRef.set_bone_position = function (player,bone,position,rotation)
+                if bone_called[bone] then
+                    coreanim.set_bone_position(player,bone,position,rotation)
+                else
+                    oldfn(player,bone,position,rotation)
+                    bone_called[bone] = true
+                end
+            end
+        else
+            ObjRef.set_bone_position = coreanim.set_bone_position
+        end
+    end
+    if mod_loaded("nc_player_model") and flags.set_bone_override then
+        local oldfn = player.set_bone_override
+        ObjRef.set_bone_override = function(player,bone,override)
+            if bone == "Head" and override and override.rotation and override.rotation.interpolation then
+                override.rotation.interpolation = nil
+            end
+            if (shim and not override_api) or bone_override then
+                coreanim.set_bone_override(player,bone,override)
             else
-                oldfn(player,bone,position,rotation)
-                bone_called[bone] = true
+                oldfn.set_bone_override(player,bone,override)
             end
         end
-    else
-        ObjRef.set_bone_position = coreanim.set_bone_position
+    elseif (shim and not override_api) or bone_override then
+        ObjRef.set_bone_override = coreanim.set_bone_override
     end
 end
 
